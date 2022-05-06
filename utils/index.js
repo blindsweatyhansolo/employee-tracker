642f1faac188a8f1b39bbba9c45865febe25feb0
J Semidey <mtsemidey@gmail.com>
2022-05-06 12:01:38 -0400

    view dept budget function finished and working

--- utils/index.js
+++ utils/index.js
@@ -158,50 +158,61 @@ const removeDepartment = () => {
 };
 
 // VIEW SALARIES BY DEPARTMENT
-// const viewDepartmentBudget = () => {
-//     // empty arrays for department data
-//     let departmentName = [];
-//     let departments = [];
+const viewDepartmentBudget = () => {
+    // empty arrays for department data
+    let departmentName = [];
+    let departments = [];
 
-//     // pull all department names for prompt
-//     const deptQuery = `SELECT name, id FROM department`;
-//     db.query(deptQuery, (err, res) => {
-//         // loop through response, push to array
-//         for (var i = 0; i < res.length; i++) {
-//             departmentName.push(res[i].name);
-//             departments.push(res[i]);
-//         }
+    // pull all department names for prompt
+    const deptQuery = `SELECT name, id FROM department`;
+    db.query(deptQuery, (err, res) => {
+        // loop through response, push to array
+        for (var i = 0; i < res.length; i++) {
+            departmentName.push(res[i].name);
+            departments.push(res[i]);
+        }
 
-//         // prompt to select which department's salaries to view based on name
-//         return inquirer
-//         .prompt(
-//             {
-//                 name: "deptBudget",
-//                 message: "Which department's budget would you like'?",
-//                 type: "list",
-//                 choices: departmentName
-//             }
-//         )
-//         .then((deptChoice) => {
-//             // get id based on name match
-//             departments.forEach((department) => {
-//                 if (department.name === deptChoice.deptBudget) {
-//                     deptChoice.deptBudget = department.id;
-//                 }
-//             });
+        // prompt to select which department's salaries to view based on name
+        return inquirer
+        .prompt(
+            {
+                name: "deptBudget",
+                message: "Which department's budget would you like'?",
+                type: "list",
+                choices: departmentName
+            }
+        )
+        .then((deptChoice) => {
+            // get department_id based on name match
+            departments.forEach((department) => {
+                if (department.name === deptChoice.deptBudget) {
+                    deptChoice.deptBudget = department.id;
+                }
+            });
 
-//             // return combined salaries of all employees in matching dept as budget
-//             const sql = `SELECT SUM(role.salary) AS Deptartment_Budget FROM role WHERE ?`;
-//             db.query(sql, {id: deptChoice.deptBudget}, (err, res) => {
-//                 console.table(res);
-//                 viewDepartmentSection();
-//             });
-//         });
-//     });  
-// };
+            // return combined salaries (SUM) of all employees in matching dept as "Budget"
+            // JOIN role_ids from employee table (fills FK role_id in employee table with data from role table)
+            // JOIN department_ids from department table (fills FK department_id in employee table with data from department table)
+            // WHERE role table's dept id matches id value from name match
+            const sql = `SELECT SUM(role.salary) 
+                        AS Budget 
+                        FROM employee
+                        LEFT JOIN role ON employee.role_id = role.id 
+                        LEFT JOIN department ON role.department_id = department.id 
+                        WHERE role.department_id = ?
+                        `;
+            db.query(sql, [deptChoice.deptBudget], (err, res) => {
+                console.table(res);
+                // return to department section
+                viewDepartmentSection();
+            });
+        });
+    });  
+};
 
 // -- ROLE FUNCTIONS -- //
 const viewRoleSection = () => {
+    // prompt to navigate role functions
     return inquirer
       .prompt({
           name: 'roleChoice',
@@ -237,11 +248,76 @@ const viewRoles = () => {
     db.query(`SELECT * FROM role`, (err, res) => {
         if (err) throw err;
         console.table(res);
+        // return to role section
         viewRoleSection();
     });
 };
 
 // ADD A ROLE
+const addRole = () => {
+    // empty array to hold department name
+    let departmentName = [];
+    // empty array to hold department name and id
+    let departments = [];
+
+    // pull all department names for prompt
+    const deptQuery = `SELECT name, id FROM department`;
+    db.query(deptQuery, (err, res) => {
+        // loop through response, push to array
+        for (var i = 0; i < res.length; i++) {
+            departmentName.push(res[i].name);
+            departments.push(res[i]);
+        }
+
+        // prompt for new role name
+        return inquirer
+        .prompt(
+            {
+            name: "roleTitle",
+            message: "What is the new role's title?",
+            validate: input => input ? true : "Role title is required."
+            },
+            {
+            name: "roleSalary",
+            message: "What is the new role's salary?",
+            validate: input => {
+                if (isNaN(input)) {
+                    return "Salary must be a number";
+                }
+                return true;
+            }
+            },
+            {
+                name: "roleDept",
+                message: "Which department does this role belong to?",
+                type: 'list',
+                choices: departmentName
+            }
+        )
+        .then((answer) => {
+            // get department id from answer
+            departments.forEach((department) => {
+                if (department.name = answer.roleDept) {
+                    answer.roleDept = department.id;
+                }
+            });
+    
+            db.query(`
+                INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
+                [
+                    answer.roleTitle, 
+                    answer.salary,
+                    answer.roleDept
+                ],
+                (err, res) => {
+                if (err) throw err;
+                console.log(`Role created`);
+                viewRoleSection();
+                })
+        });
+    })
+
+}
 // DELETE A ROLE
 
 // -- EMPLOYEE FUNCTIONS -- //
